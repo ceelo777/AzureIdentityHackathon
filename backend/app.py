b208ef8d34d0f16b841b2f6281b0a1d791978eec
Sarah Gu <t-sarahgu@microsoft.com>
2022-06-29 16:39:31 -0700

    added endpoints for flagging emails

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request
 import os
+import configparser
+from graph import Graph
+from main import greet_user
 # import tensorflow as tf 
 # from tensorflow import keras 
 # from keras.models import load_model
@@ -22,6 +25,42 @@ def receive():
         print(request.json['text'])
 
     return "Hello, World!"
+
+@app.route("/parseInbox", methods=["POST"])
+@cross_origin()
+def flaggedEmails(): 
+    #receives array of emails, returns two dictionaries of flagged vs. unflagged
+    model = load_model("mymodel.h5")
+    model._make_predict_function()
+    good_emails = []
+    bad_emails = []
+
+    if request.method == 'POST': 
+        myEmails = request.json['text']
+
+        for email in myEmails: 
+            preds = model.predict(email)
+            if preds[0][0] > 0.5: 
+                bad_emails.append(email)
+            else: 
+                good_emails.append(email)
+
+    return (good_emails, bad_emails)
+
+@app.route("/showInbox", methods=["POST"])
+@cross_origin()
+def showEmails(): 
+    config = configparser.ConfigParser()
+    config.read(['config.cfg', 'config.dev.cfg'])
+    azure_settings = config['azure']
+
+    graph: Graph = Graph(azure_settings)
+
+    greet_user(graph)
+
+    return graph.get_inbox()
+
+
 
 if __name__ == "__main__":
     app.run()
