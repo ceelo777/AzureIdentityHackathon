444eef36fec524d6b3d995990b275371be748ea4
Christopher Lo <t-clo@microsoft.com>
2022-06-29 13:15:37 -0700

    add api endpoint and make input form

--- frontend/src/InputPage/InputPage.js
+++ frontend/src/InputPage/InputPage.js
@@ -17,7 +17,14 @@ class InputPage extends React.Component {
     }
   
     handleSubmit(event) {
-      alert('An email was submitted: ' + this.state.value);
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify( { text: this.state.value } )
+      };
+      fetch('http://localhost:5000/submitText', requestOptions)
+        .then(response => response.json())
+        .then(data => console.log(data));
       event.preventDefault();
     }
   
